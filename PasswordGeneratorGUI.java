0e84525acadfe9e23c02dd1aafa03e561b7ac90e
Mark <39029724+KrAm211@users.noreply.github.com>
2026-03-15 14:44:59 +0000

    Changes

--- PasswordGeneratorGUI.java
+++ PasswordGeneratorGUI.java
@@ -5,42 +5,77 @@ import java.util.Random;
 public class PasswordGeneratorGUI {
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Password Generator");
-
-        frame.setSize(400, 150);
+        JFrame frame = new JFrame("NEXUS - Password Generator");
+        frame.setSize(450, 175);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setLayout(new FlowLayout());
 
         JLabel lengthLabel = new JLabel("Password Length:");
         JTextField lengthField = new JTextField(10);
-
+        JCheckBox lowercaseBox = new JCheckBox("Lowercase", true);
+        JCheckBox uppercasBox = new JCheckBox("Uppercase", true);
+        JCheckBox numbersBox = new JCheckBox("Numbers", true);
+        JCheckBox symbolsBox = new JCheckBox("Symbols", true);
         JButton generateButton = new JButton("Generate Password");
-
-        JTextField passwordfField = new JTextField(20);
+        JTextField passwordfField = new JTextField(25);
         passwordfField.setEditable(false);
 
-        frame.add(lengthLabel);
         frame.add(lengthField);
+        frame.add(lengthLabel);
+        frame.add(lowercaseBox );
+        frame.add(uppercasBox);
+        frame.add(numbersBox);
+        frame.add(symbolsBox);
         frame.add(generateButton);
         frame.add(passwordfField);
 
+        Random random = new Random();
+
         generateButton.addActionListener(e -> {
-
             String lengthText = lengthField.getText();
-            int length = Integer.parseInt(lengthText);
 
-            String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+            int length;
 
-            Random random = new Random();
+            try {
+                length = Integer.parseInt(lengthText);
+            } catch (NumberFormatException ex) {
+                passwordfField.setText("Please enter numbers only");
+                return;
+            }
+
+            if (length < 12 || length > 32) {
+                passwordfField.setText("Length must be between 12 and 32");
+                return;
+            }
+
+            StringBuilder characters = new StringBuilder();
+
+            if (lowercaseBox.isSelected()) {
+                characters.append("abcdefghijklmnopqrstuvwxyz");
+            }
+            if (uppercasBox.isSelected()) {
+                characters.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            }
+            if (numbersBox.isSelected()) {
+                characters.append("0123456789");
+            }
+            if (symbolsBox.isSelected()) {
+                characters.append("!@#$%^&* ");
+            }
+            if (characters.length() == 0) {
+                passwordfField.setText("Select at least one character type");
+            }
+
             StringBuilder password = new StringBuilder();
 
             for (int i = 0; i < length; i++) {
-                System.out.println("I = " + Integer.toString(i));
+                //Print for loop position for debugging
+                //System.out.println("I = " + Integer.toString(i));
                 int index = random.nextInt(characters.length());
                 char randomCharacter = characters.charAt(index);
                 password.append(randomCharacter);
-                System.out.println("Password: " + password.toString());
+                //Print password at this step for debugging
+                //System.out.println("Password: " + password.toString());
             }
 
             passwordfField.setText(password.toString());
